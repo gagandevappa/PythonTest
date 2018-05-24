8a25643b6f23bbd21ae70daa25a5ea149fdfd1d8
Gagan Devappa <gagan.devappa@tecnotree.com>
2018-05-24 17:57:05 +0530

    committing after mathematical excerise

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,10 @@
 print("Hello Gagan")
 print("How are you")
 print("keep calm and learn python!!")
+#print("Good Bye")
+#ans:30
+print("Hens:",25+30/6)
+#the total egg count is 6.75
+print(3+2+1-5+4%2-1/4+6)
+#true
+print("Is it greater or equal?", 5 >= -2)

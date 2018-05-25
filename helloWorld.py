6ffb3d89e0fd5e6497cc73c4ebe2a389949d36bc
Gagan Devappa <gagan.devappa@tecnotree.com>
2018-05-25 12:10:57 +0530

    committing after mathematical excerise

--- helloWorld.py
+++ helloWorld.py
@@ -1,10 +1,48 @@
+#print syntax
 print("Hello Gagan")
 print("How are you")
 print("keep calm and learn python!!")
+
+#comment
 #print("Good Bye")
+
+#numbers and math
 #ans:30
 print("Hens:",25+30/6)
 #the total egg count is 6.75
 print(3+2+1-5+4%2-1/4+6)
 #true
 print("Is it greater or equal?", 5 >= -2)
+
+#variables and names
+cars = 100
+space_in_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars-drivers
+cars_driven = drivers
+carpool_capacity = cars_driven*space_in_car
+average_passangers_in_a_car = passengers/cars_driven
+print("There are ",cars," cars available")
+print("There are only ",drivers," drivers available")
+print("There are total of ",passengers," passengers")
+print("we have car pool capacity of ",carpool_capacity)
+print("we need to put ",average_passangers_in_a_car," passangers in  each car")
+
+#formatted strings
+name="Gagan ND"
+height=170
+weight=72
+eye="black"
+occupation="software Engineer"
+print(f"lets talk about {name}")
+print(f"he is {height} cm tall")
+print(f"his weight is of {weight}")
+print(f"he has {eye} eyes")
+print(f"he is an {occupation}")
+total=height+weight
+print(f"if you add up  {height} and  {weight}: the total will be {total}.")
+
+#strings and Text
+
+

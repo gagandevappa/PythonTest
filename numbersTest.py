d712028d48315b3af2e864aafd69f3786100f878
Gagan Devappa <gagan.devappa@tecnotree.com>
2018-09-03 11:35:24 +0530

    strings and numerial operations

--- numbersTest.py
+++ numbersTest.py
@@ -5,3 +5,26 @@ print(3+3)
 print(9//2)
 print(4**2)
 print(9%2)
+print(3*3+5)
+print(3*(3+5))
+num=2
+num*=10
+print(num)
+test=-5
+print(abs(test))
+print(round(3.4568,2))
+#comparision
+num_1=10
+num_2=20
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1<=num_2)
+
+#cast
+num_1='10'
+num_2='30'
+num_3=int(num_1)
+num_4=int(num_2)
+print(num_3+num_4)
+print(num_1+num_2)

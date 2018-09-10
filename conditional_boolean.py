2125c1ff95a0ab1ae8e9ff1d85a1bf8fee8da5db
Gagan Devappa <gagan.devappa@tecnotree.com>
2018-09-10 12:40:21 +0530

    adding the if-else eample

---
+++ conditional_boolean.py
@@ -0,0 +1,8 @@
+language='python'
+if language =='Java':
+    print('The lanuage entered: '+language+' but expected: java')
+elif language=='python':
+    print('The lanuage entered: '+language+' but expected: python')
+else:
+    print('The lanugae is unknown')
+

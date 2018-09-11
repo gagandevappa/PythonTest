079267e65ffd5464dc34de2ba6d6a94ffebff125
Gagan Devappa <gagan.devappa@tecnotree.com>
2018-09-11 13:23:36 +0530

    added few examples

--- python_basics/file_handle/fileTest_1.py
+++ python_basics/file_handle/fileTest_1.py
@@ -10,6 +10,13 @@ with open('testdata.txt', 'r') as f:
     val3=f.read()
     print(val3)
 
+print("\n******context-manager to read line by line*************")
+with open('testdata.txt', 'r') as f:
+    val4=f.readline()
+    print(val4)
+    f.seek(50)
+    val4=f.readline()
+    print(val4)
 
 print("\n******context-manager- write*************")
 with open('testwrite.txt', 'w') as w:
@@ -19,9 +26,17 @@ with open('testwrite.txt', 'w') as w:
 print("*******file copy*********")
 with open('testdata.txt', 'r') as rd:
     with open('testwrite-copy.txt', 'w') as wr:
-        val1=rd.read()
+        val1=rd.readline()
         wr.write(val1)
 
+print("*******file copy with chunk*********")
+with open('testdata.txt', 'r') as rd:
+    with open('testwrite-copy.txt', 'w') as wr:
+        chunk=10
+        val1=rd.read(chunk)
+        while len(val1) > 0:
+            wr.write(val1)
+            val1=rd.read(chunk)
 
 print("*******copy an image with chunk-size*********")
 with open('Digital-Commerce.png', 'rb') as rb:

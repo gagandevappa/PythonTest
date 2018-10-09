edcbeb26779dbb8ba369ff17598ec33b1d2103f0
Gagan Devappa <gagan.devappa@tecnotree.com>
2018-10-09 12:15:20 +0530

    added the loop condition

---
+++ python_basics/sql_tutorial/1_sqltest.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+my_name = 'Adesh'
+if(my_name == 'Gagan'):
+    print("Hello "+my_name+" welcome to Bengaluru!!")
+else:
+    print("The name: "+my_name+" is incorrect!! please check")
